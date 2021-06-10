c231a8dab293ab44e475113749ccc12d79912672
stanislav zaluzhsky <stanislav.zaluzhsky@gmail.com>
2021-06-10 11:35:51 +0300

    Rework of XPath expression init

--- src/main/java/com/example/datstestprj/services/CBRFCurrencyExchangeService.java
+++ src/main/java/com/example/datstestprj/services/CBRFCurrencyExchangeService.java
@@ -7,17 +7,19 @@ import com.example.datstestprj.types.CurrencyType;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
+import javax.annotation.PostConstruct;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import java.io.IOException;
@@ -32,21 +34,41 @@ import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Slf4j
 public class CBRFCurrencyExchangeService {
-    StringBuilder cbrURL = new StringBuilder("http://www.cbr.ru/scripts/XML_daily.asp?date_req=");
-    DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-    HttpClient httpClient = HttpClient.newBuilder()
+    Map<CurrencyType, XPathExpression> xPathCurrency;
+    final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+    final String cbrURL = new String("http://www.cbr.ru/scripts/XML_daily.asp?date_req=");
+    final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    final HttpClient httpClient = HttpClient.newBuilder()
             .version(HttpClient.Version.HTTP_1_1)
             .build();
-    DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
-    XPath xPath = XPathFactory.newInstance().newXPath();
-    String expression = "//CharCode[text()=\"{0}\"]/../Value";
-    CurrencyRepository currencyRepository;
-    List<NotificationService> notificationServices;
+    final String expression = "//CharCode[text()=\"{0}\"]/../Value";
+    final CurrencyRepository currencyRepository;
+    final List<NotificationService> notificationServices;
+
+    @PostConstruct
+    private void init() throws XPathExpressionException {
+        try {
+            xPathCurrency = Map.of(
+                    CurrencyType.EUR, XPathFactory.newInstance().newXPath().compile(
+                            MessageFormat.format("//CharCode[text()=\"{0}\"]/../Value", CurrencyType.EUR)),
+                    CurrencyType.GBP, XPathFactory.newInstance().newXPath().compile(
+                            MessageFormat.format("//CharCode[text()=\"{0}\"]/../Value", CurrencyType.GBP)),
+                    CurrencyType.USD, XPathFactory.newInstance().newXPath().compile(
+                            MessageFormat.format("//CharCode[text()=\"{0}\"]/../Value", CurrencyType.USD))
+            );
+        } catch (XPathExpressionException e) {
+            log.error(e.getMessage(), e);
+            throw e;
+        }
+    }
 
     public String getExchangeRate(CurrencyType currencyType, LocalDate dateOn) {
        Currency currency = currencyRepository.findCurrencyBySourceCurrencyAndDateOnEquals(currencyType, dateOn);
@@ -62,7 +84,7 @@ public class CBRFCurrencyExchangeService {
     public void parse(LocalDate processingDate) throws IOException, InterruptedException, SAXException, ParserConfigurationException, XPathExpressionException, ParseException {
         processingDate =  processingDate == null ? LocalDate.now() : processingDate;
         String strDate = processingDate.format(DATE_FORMAT);
-        String url = cbrURL.append(strDate).toString();
+        String url = cbrURL.concat(strDate);
 
         HttpRequest request = HttpRequest.newBuilder()
                 .GET()
@@ -71,12 +93,12 @@ public class CBRFCurrencyExchangeService {
 
         HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
-        DocumentBuilder builder = builderFactory.newDocumentBuilder();
+        DocumentBuilder builder = dbf.newDocumentBuilder();
         Document xmlDocument = builder.parse(new InputSource(new StringReader(response.body())));
         for (CurrencyType currencyType : CurrencyType.values()) {
             //skip RUB
             if (currencyType != CurrencyType.RUB) {
-                String value = (String) xPath.compile(MessageFormat.format(expression, currencyType)).evaluate(xmlDocument, XPathConstants.STRING);
+                String value = (String) xPathCurrency.get(currencyType).evaluate(xmlDocument, XPathConstants.STRING);
 
                 Number rate = NumberFormat.getNumberInstance().parse(value.replace(',', '.'));
 
